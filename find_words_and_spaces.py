69db96a8e893d6b1463ba653763d36d416767b8c
J. Cohen <hostelmarketing@gmail.com>
2015-10-15 01:07:12 -0700

    Improve the script.

--- find_words_and_spaces.py
+++ find_words_and_spaces.py
@@ -1,42 +1,55 @@
+"""Takes an alphabetic string and returns all words with any adjacent spaces."""
 import re
 
-def has_leading_space(word):
+
+# The text to process. This could be read from a file.
+s = 'Saluton, Mondo! Mi estas komputilo.'
+print('Original text: {}'.format(s))
+
+
+def has_leading_space(word, text):
     """Returns True if word has a leading space."""
     word_start = text.index(word)
     return text[word_start-1] == ' '
 
-def has_trailing_space(word):
+
+def has_trailing_space(word, text):
     """Returns True if word has a trailing space."""
     word_start = text.index(word)
     word_end = word_start + len(word)
     return text[word_end] == ' '
 
-# The text to process
-text = 'Saluton, Mondo! Mi estas komputilo.'
-print('Original text: {}'.format(text))
 
-# Extracts words without punctuation
-p = re.compile(r'(\b[a-zA-Z]{1,}\b)')
+def extract_words(text):
+    # Extracts words without punctuation
+    p = re.compile(r'(\b[a-zA-Z]{1,}\b)')
+    words = re.findall(p, text)
+    print('Extracted words: {}'.format(words))
+    return words
 
-words = re.findall(p, text)
-print('Extracted words: {}'.format(words))
 
-words_with_spaces = []
+def pad_words(words, text):
+    words_with_spaces = []
 
-for word in words:
-    start = text.index(word[0])
-    end = text.index(word[len(word)-1])
+    for word in words:
+        word_index = text.index(word)
+        leading_space = has_leading_space(word, text)
+        trailing_space = has_trailing_space(word, text)
+        print('Word: "{}", Leading: {}, Trailing: {}'.format(word, leading_space, trailing_space))
+        if leading_space is True:
+            word = ' {}'.format(word)
+        if trailing_space is True:
+            word = '{} '.format(word)
+        words_with_spaces.append(word)
+    print('Words with spaces: {}'.format(words_with_spaces))
+    return words_with_spaces
 
-    word_index = text.index(word)
 
-    leading_space = has_leading_space(word)
-    trailing_space = has_trailing_space(word)
-    print('Word: {}, Leading: {}, Trailing: {}'.format(word, leading_space, trailing_space))
-    if leading_space is True:
-        word = ' {}'.format(word)
-    if trailing_space is True:
-        word = '{} '.format(word)
-    words_with_spaces.append(word)
+def main():
+    """Runs the program."""
+    pad_words(extract_words(s), s)
 
-print('Words with spaces: {}'.format(words_with_spaces))
+
+if __name__ == '__main__':
+    main()
 
